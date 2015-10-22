61810ed9aed087af656473eda58c389a8f2d9ab7
Michal Špaček <tupinek@gmail.com>
2015-10-22 19:45:31 +0200

    Prepare for 0.03 version.

--- List.pm
+++ List.pm
@@ -16,7 +16,7 @@ Readonly::Scalar our $EMPTY_STR => q{};
 Readonly::Scalar our $SPACE => q{ };
 
 # Version.
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 # Constructor.
 sub new {
@@ -310,6 +310,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
